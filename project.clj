d0004e0aa7f07d9aa3e4fc7f3b00fffaf9535891
Brad Ediger <brad.ediger@madriska.com>
2014-06-02 11:11:00 -0500

    EPL license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject access_access "0.0.1-SNAPSHOT"
   :description "Suck data out of an Access database"
   :url "http://example.com/FIXME"
-  :license {:name "Proprietary"
-            :url ""}
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clojess "0.3"]
                  [org.clojure/data.csv "0.1.2"]]
